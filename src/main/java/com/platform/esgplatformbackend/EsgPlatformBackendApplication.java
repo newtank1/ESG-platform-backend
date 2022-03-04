6105f7953fcd1d792ccd97a3c8f5d40f761cff17
newtank1 <727786943@qq.com>
2022-03-04 21:54:17 +0800

    init

---
+++ src/main/java/com/platform/esgplatformbackend/EsgPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.platform.esgplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsgPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platform/esgplatformbackend/EsgPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.esgplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
